f89b4efba4f627382376a8e34fb65a8eb36c9318
fas734 <af_l8g1011@mail.ru>
2018-12-04 10:46:24 +0700

    evaluates nabl_x

--- multi-rang-find/gradient.py
+++ multi-rang-find/gradient.py
@@ -19,15 +19,29 @@ x = dict()		# access to each component is through symbolic key
 for var_name in x_sym:
 	x[str(var_name)] = random.randint(0, 22) - 13	# random value of x components
 
-f_sym = 4 * (x1-5)**2 + 70 * (x2-2)**2 + 31
+f_sym = 4 * (x1-5)**2 + 70 * (x2-2)**2 + 31	# symbolic function
 
 print("\n-------------------------\nSTART Gradient method")
+
+print("X[0]")
 for item in x.items():
 	print(item)
+
+print("keys of dict")
+for key in x.keys():
+	print(key)
+
+print("values of dict")
+for key in x.values():
+	print(key)
 
 nabl_x = dict()
 for key in x.keys():
 	nabl_x[key] = sympy.diff(f_sym, key, 1)
+	print(nabl_x[key])
 
+for key in nabl_x.keys():
+	print("nabl_x['", key, "'] := ", nabl_x[key], " = ", nabl_x[key].subs({key:x[key]}), sep='')
+########################## PYTHON HISTORY: .python_history #######################
 # print(f_sym)
 # print(nabl_x)
